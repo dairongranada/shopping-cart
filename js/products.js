3353fb42cd248c77fb923f285283a03053c6d244
Dairon Granada <93607018+dairongranada@users.noreply.github.com>
2022-05-11 18:44:14 -0500

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,10 @@
+const nikeForce = [
+  {id:"NikeForceOne-01",product: 'Air Force One Experimental',price: 249.999,image: '/img/AirForce1-Experimental.jpg'}, 
+  {id:"NikeForceOne-02",product: 'Air Force One Flyleather',price: 239.999,image: '/img/AirForce1-Flyleather .jpg'},
+  {id:"NikeForceOne-03",product: 'Air Force One Low',price: 149.999,image: '/img/AirForce1-Low.jpg'},
+  {id:"NikeForceOne-04",product: 'Air Force One Skeleton',price: 250.999,image: '/img/AirForce1-Skeleton.jpg'},
+  {id:"NikeForceOne-05",product: 'Air Force One 107',price: 139.999,image: '/img/AirForce-107.jpg'},
+  {id:"NikeForceOne-06",product: 'Air Force One AN20',price: 299.999,image: '/img/AirForce-AN20.jpg'},
+]; 
+
+console.log(Object.values(nikeForce));

7841f3c2d4ea5ec4eb6abf6c36ea1fc8e0e10bf5
Dairon Camargo Granada <93607018+dairongranada@users.noreply.github.com>
2022-05-26 17:38:57 -0500

    NikeShop Finish (Version - 2.4.5).DG

--- js/app.js
+++ js/app.js
@@ -16,6 +16,9 @@ const btnAddCard =  document.getElementById("btnAddCard");
 
 ///////////////////////////////////////////////////////////////
 const filterProducts = document.getElementById("filterProducts");
+const divNumberAument = document.getElementById("divNumberAument")
+const numberAument = document.getElementById("numberAument")
+const comingSoonShop = document.getElementById("comingSoon")
 
 ////////////////      EVENTOS         /////////////////
 selectProducts.addEventListener('change', renderCards);
@@ -52,10 +55,8 @@ function importImg(event){ //Funcion Para Crear URL a la img En el local
 }
 
 
-
 function funcBtnAddCard(event){ //CAPTURA EL TITULO, PRECIO Y LA IMG 
   idFinish = idTextShoess + (idNumShoess++);
-
   const shoesAdd = {
     id: idFinish,
     product: nameShoes.value,
@@ -63,7 +64,7 @@ function funcBtnAddCard(event){ //CAPTURA EL TITULO, PRECIO Y LA IMG
     image: imgSelected
   }
   nikeForceOne.unshift(shoesAdd);
-  nikeForceOne.forEach(element => {console.log(element);});
+  // nikeForceOne.forEach(element => {console.log(element);});
   listSelect();
 
   ////////////////    S T Y L O S   ////////////////
@@ -95,8 +96,6 @@ function renderCards() {
 }
 
 function listSelect() {
-    let optionS = document.createElement('option');
-    optionS.textContent ="SELECT YOUT SHOES";
     selectProducts.innerHTML = "";
     nikeForceOne.map (element =>{
 
@@ -125,13 +124,22 @@ function createCards(nikeForceOne) {
     let priceProduct = document.createElement('p');
     priceProduct.classList.add('price-product');
     priceProduct.textContent = price; 
-    
+
+
     let btnAdd = document.createElement('button');
     btnAdd.setAttribute('id',id);
-    btnAdd.classList.add('btn-add');
-    btnAdd.textContent = 'COMPRAR';
-    btnAdd.addEventListener('click', funtionShop)
+    btnAdd.classList.add('btnAddCart');
+    btnAdd.textContent = 'ADD TO CART';
+    btnAdd.addEventListener('click', funtionShop);
 
+    let btnShopCart = document.createElement('button');
+    btnShopCart.setAttribute('id',id);
+    btnShopCart.classList.add('btnShopCart');
+    btnShopCart.textContent = 'COMPRAR';
+    btnShopCart.addEventListener('click', comingSoon);
+
+
+  
     let btnDelete = document.createElement('button');
     btnDelete.setAttribute('id', id);
     btnDelete.classList.add('deleteCards');
@@ -147,9 +155,21 @@ function createCards(nikeForceOne) {
     contentCard.appendChild(nameProduct);
     contentCard.appendChild(priceProduct); 
     contentCard.appendChild(btnAdd);
+    contentCard.appendChild(btnShopCart);
     contentCard.appendChild(btnDelete);
     main.appendChild(contentCard);
 }
+
+/////////////////////////////////////////////////////////////////////////////////
+// ===================      P R O X I M A M E N  T  E   =================== //
+
+function comingSoon() {
+  comingSoonShop.style.visibility = "visible";
+  CloseComingSoon = document.getElementById('CloseComingSoon');
+  const CloseComing = () => {comingSoonShop.style.visibility = "hidden";}
+  CloseComingSoon.addEventListener('click', CloseComing);
+}
+
 
 /////////////////////////////////////////////////////////////////////////////////
 // ========      A G R E G A R   C A R R I T O   D E   C O M P R A S   ======== //
@@ -161,10 +181,20 @@ function funtionShop(event){
   })
 }
 
-let total = 0;
-let add = 1;
+// let total = 0;
+let sh1 = 1; //let sh2= 1;let sh3= 1;let sh4= 1;let sh5= 1;let sh6= 1;let sh7= 1;let sh8= 1;let sh9= 1;let sh10 = 1;
+
+arrayProducts = [];
+let incremet = 0;
+let nA = 1;
+
 function createShop(nikeForceOne){
   const {id,product,price,image} = nikeForceOne;
+  arrayProducts.push(id);
+
+  arrayProducts.forEach(element => {
+        if(element === id){incremet++; console.log(element);}
+  });
 
   let contentCard = document.createElement('div');
   contentCard.classList.add('productShoesRecord');
@@ -173,100 +203,140 @@ function createShop(nikeForceOne){
   let labelNameS = document.createElement('p');
   labelNameS.textContent = "Name Shoes";
   let nameS = document.createElement('p');
-  nameS.classList.add('nameShoesRecord');
-  nameS.textContent = product;
-  nameS.textContent = product;
+  nameS.setAttribute('id',id)
+    nameS.classList.add('nameShoesRecord');
+    nameS.textContent = product;
 
   let div2 = document.createElement('div');
   let labelpriceS = document.createElement('p');
   labelpriceS.textContent = "Price";
   let priceS = document.createElement('p');
-  priceS.classList.add('priceShoesRecord');
-  priceS.textContent = price; 
+    priceS.setAttribute('id',id)
+    priceS.classList.add('priceShoesRecord');
+    priceS.textContent = price; 
 
   let div3 = document.createElement('div');
+  div3.setAttribute('id','contentAmountS')
   let labelAmountS = document.createElement('p');
   labelAmountS.textContent = "Amount";
   let amountS = document.createElement('p');
-  amountS.setAttribute('id',id)
-  amountS.classList.add('amountShoesRecord');
-  amountS.textContent = 1; 
+    amountS.setAttribute('id',id);
+    let contenAmount = amountS.id;
+    amountS.classList.add('amountShoesRecord');
+    amountS.textContent = 1; 
 
   let divbtn1M = document.createElement('div');
   let labeldivbtn1MS = document.createElement('p');
   labeldivbtn1MS.textContent = "Add";
   let btn1M = document.createElement('button');
-  btn1M.setAttribute('id',id)
-  btn1M.classList.add('addShoesRecord');
-  btn1M.textContent = "+"; 
+    btn1M.setAttribute('id',id)
+    btn1M.classList.add('addShoesRecord');
+    btn1M.textContent = "+"; 
 
   let divbtn2M = document.createElement('div');
   let labeldivbtn2MS = document.createElement('p');
   labeldivbtn2MS.textContent = "Erase";
   let btn2M = document.createElement('button');
-  btn2M.setAttribute('id',id)
-  btn2M.classList.add('eraseShoesRecord');
-  btn2M.textContent = "-"; 
+    btn2M.setAttribute('id',id)
+    btn2M.classList.add('eraseShoesRecord');
+    btn2M.textContent = "-"; 
   
   let div4 = document.createElement('div');
   let imgS = document.createElement('img');
-  imgS.setAttribute('src',image);
-  imgS.classList.add('imgShoesRecord');
+    imgS.setAttribute('id',id)
+    imgS.setAttribute('src',image);
+    imgS.classList.add('imgShoesRecord');
 
 
-    div1.appendChild(labelNameS);
-    div1.appendChild(nameS);
+    if (incremet === 1) {
+      div1.appendChild(labelNameS);
+      div1.appendChild(nameS);
 
-    div2.appendChild(labelpriceS);
-    div2.appendChild(priceS);
+      div2.appendChild(labelpriceS);
+      div2.appendChild(priceS);
 
-    div3.appendChild(labelAmountS);
-    div3.appendChild(amountS);
+      div3.appendChild(labelAmountS);
+      div3.appendChild(amountS);
 
-    div4.appendChild(imgS);
+      div4.appendChild(imgS);
 
-    divbtn1M.appendChild(labeldivbtn1MS)
-    divbtn1M.appendChild(btn1M)
+      divbtn1M.appendChild(labeldivbtn1MS)
+      divbtn1M.appendChild(btn1M)
 
-    divbtn2M.appendChild(labeldivbtn2MS)
-    divbtn2M.appendChild(btn2M)
+      divbtn2M.appendChild(labeldivbtn2MS)
+      divbtn2M.appendChild(btn2M)
 
-    contentCard.appendChild(div1);
-    contentCard.appendChild(div2);
-    contentCard.appendChild(div3);
-    contentCard.appendChild(divbtn1M);
-    contentCard.appendChild(divbtn2M);
-    contentCard.appendChild(div4);
-    contenProducts.appendChild(contentCard);
+      contentCard.appendChild(div1);
+      contentCard.appendChild(div2);
+      contentCard.appendChild(div3);
+      contentCard.appendChild(divbtn1M);
+      contentCard.appendChild(divbtn2M);
+      contentCard.appendChild(div4);
+      contenProducts.appendChild(contentCard);
+
+      numberAument.textContent = nA++;
+      divNumberAument.style.backgroundColor = 'red';
+  
+  } incremet=0;
+    
 
 
+    //  ========== BOTON DE AUMENTO ==========
 
     btn1M.addEventListener("click", aggMoreValue);
     function aggMoreValue() {
-      amountS.textContent = add++;
-      total = nikeForceOne.price * add;
-      priceS.textContent = total;
+      if (btn1M.id === contenAmount) {
+        amountS.textContent = sh1++;
+      }
     }
-
-
+    //  ========== BOTON DE DES AUMENTO ==========
+    btn2M.addEventListener("click", noMoreValue);
+      function noMoreValue() {
+        if (btn2M.id === contenAmount) {
+          amountS.textContent = sh1--;
+          console.log(amountS.value);
+        }
+      }
 }
 
 
-///////////////////////////////////////////////////////////////////////////////////
-// ========     B  O  T  O  N       D  E     (D  E S )  A  U  M  E  N  T  O         ======== //
 
 
-// incrementt.addEventListener('click', funcIncrement);
-// decreasee.addEventListener('click', funcDecrease);
-// let add = 1;
-// let result = 0
-// function funcIncrement() {
-//   amuntt.textContent = add++;
-// }
 
-// function funcDecrease() {
-//   amuntt.textContent = add-- -(1);
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //  ========== BOTON DE AUMENTO  MAS QUEMADO QUE EL **** ==========
+    // btn1M.addEventListener("click", aggMoreValue);
+    // function aggMoreValue() {
+    //   btn1M.id  == "NikeShoes-1" ? amountS.textContent = sh1++
+    //   :btn1M.id == "NikeShoes-2" ? amountS.textContent = sh2++
+    //   :btn1M.id == "NikeShoes-3" ? amountS.textContent = sh3++
+    //   :btn1M.id == "NikeShoes-4" ? amountS.textContent = sh4++
+    //   :btn1M.id == "NikeShoes-5" ? amountS.textContent = sh5++
+    //   :btn1M.id == "NikeShoes-6" ? amountS.textContent = sh6++
+    //   :btn1M.id == "NikeShoes-7" ? amountS.textContent = sh7++
+    //   :btn1M.id == "NikeShoes-8" ? amountS.textContent = sh8++
+    //   :btn1M.id == "NikeShoes-9" ? amountS.textContent = sh9++
+    //   :btn1M.id == "NikeShoes-10" ? amountS.textContent = sh10++
+    //   :null;
+    //   } 
+
+
+    
+
+
+
 // /////////////////////////////////////////////////////////////////////////////////
 // // ========      L O G O   C A R R I T O   D E   C O M P R A S       ======== //
 // const shopCartContents = document.getElementById("ShopCartContent");
@@ -277,7 +347,7 @@ function createShop(nikeForceOne){
 
 // btnCart.addEventListener("click", funCartRecord)
 // closeShoesRecord.addEventListener("click", funCartclose)
-// function funCartRecord() { //Funcion Para que Aparezca El historias De Compra
+// function funCartRecord() { //Funcion Para que Aparezca El historial De Compra
 //   shopCartContents.style.visibility = "visible";
 //   efecttBlur.style.visibility = "visible";
 // }
